b9c19fecb1eb9c7df777adb20f04d621596f6d5f
PhongNDH <phongndhdev@gmail.com>
2024-05-06 22:35:37 +0700

    initial commit

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,7 +1,27 @@
 import "./home.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { login } from "../../app/features/userLoginSlice";
 
 const Home = () => {
-   return <div>Home</div>;
+   const dispatch = useDispatch();
+   const userLogin = useSelector((state) => state.userLogin);
+   const { userInfo } = userLogin;
+
+   const handleLogin = () => {
+      console.log("LOGIN");
+      dispatch(login({}));
+   };
+   const handleGetUser = () => {
+      console.log(userLogin);
+   };
+
+   return (
+      <div>
+         Home
+         <button onClick={handleLogin}>Login</button>
+         <button onClick={handleGetUser}>User</button>
+      </div>
+   );
 };
 
 export default Home;
